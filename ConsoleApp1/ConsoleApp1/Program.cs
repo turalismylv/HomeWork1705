9ff0a3704f42dfbd1ed4e92037a5bc3ced595fd8
turalismylv <turalhi@code.edu.az>
2022-05-17 14:49:21 +0400

    HomeWork 1705

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //            //
+            //            Tapşırıq, stringdə olan adları tərs çevirib yeni bir string kimi geriyə qaytaran method yazın. Məsələn:
+            //“Nurlan Faiq Emil Tural Yusif” ->
+            //“nalruN qaiF limE laruT fisuY
+
+            //Not(hazır metodlardan kömək almaq olar, amma reverse metodundan istifadə etmək olmaz)
+
+            string name = "Nurlan Faiq Emil Tural Yusif";
+            Console.WriteLine(Revers(name));
+        }
+
+        static string Revers(string name)
+        {
+            string reversedName = "";
+            for (int i = name.Length - 1; i >= 0; i--)
+            {
+                reversedName += name[i];
+
+            }
+            return reversedName;
+
+
+        }
+
+    }
+}
+  
